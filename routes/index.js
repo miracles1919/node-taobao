49176b4200e7215c88b90095c6a74515776e0068
miracles <miracles@fanfandata.com>
2018-04-22 15:25:22 +0800

    add the goods details

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 import { login, register } from '../controller/user'
-
+import { shopDetail } from '../controller/shop'
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,5 +10,7 @@ router.get('/', function(req, res, next) {
 router.post('/login', login)
 
 router.post('/register', register)
+
+router.get('/detail/:gid', shopDetail)
 
 module.exports = router;
